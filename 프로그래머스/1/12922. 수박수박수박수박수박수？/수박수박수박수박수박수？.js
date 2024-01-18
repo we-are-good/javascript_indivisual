8319a505b51f0f88f0bf3fb40fa0a89fe61c2bd4
we-are-good <140328307+we-are-good@users.noreply.github.com>
2024-01-18 09:37:49 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    var half = parseInt(n/2);
+    var low = n%2;
+    var answer = '';
+    for (i=0; i<half; i++){
+            answer += "수박"}
+    low === 0 ? answer : answer+="수"    
+    return answer;
+}
